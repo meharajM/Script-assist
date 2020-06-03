4709499515f7743467ba92c3d034ceb525f45cb3
meharaj.moulasab <meharaj.moulasab@automationanywhere.com>
2020-06-03 14:12:34 +0530

    added basic editor

---
+++ src/containers/App/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header center">
+       
+        
+      </header>
+      <div className="App-container center">
+        {/* <textarea className="page"></textarea> */}
+        <div className="page" contentEditable></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
